87c6ff74f342470199ef72c96a3a3234ccf33f0f
ArchonWu <archonuwu@gmail.com>
2024-07-04 20:31:43 -0700

    nerve bug fix

--- Utilities/Functions.py
+++ Utilities/Functions.py
@@ -3,6 +3,10 @@ import os
 import aiohttp
 
 
+full_energy_notified = 0
+full_nerve_notified = 0
+
+
 async def request_all_player_bars():
     torn_api_key_limited = os.getenv('TORN_API_KEY_LIMITED')
     url = f"https://api.torn.com/user/?selections=bars&key={torn_api_key_limited}"
@@ -11,7 +15,7 @@ async def request_all_player_bars():
             return await resp.json()
 
 
-# calculate whether user's energy or nerve level reaches target_level
+# calculate whether user's energy or nerve level reaches target_level, and notify the user
 # e.g. 80/100 is >= 0.8
 async def check_energy_or_nerve_reach_levels(target_user, target_level):
     torn_user_bars = await request_all_player_bars()
@@ -21,23 +25,25 @@ async def check_energy_or_nerve_reach_levels(target_user, target_level):
     user_energy_current = torn_user_bars["energy"]["current"]
     user_energy_maximum = torn_user_bars["energy"]["maximum"]
     user_energy_level = user_energy_current / user_energy_maximum
+    user_energy_level_rounded = round(user_energy_level * 100, 3)
     print(user_energy_level, user_energy_current, user_energy_maximum)
 
     # calculate user_nerve_level
     user_nerve_current = torn_user_bars["nerve"]["current"]
     user_nerve_maximum = torn_user_bars["nerve"]["maximum"]
     user_nerve_level = user_nerve_current / user_nerve_maximum
+    user_nerve_level_rounded = round(user_nerve_level * 100, 3)
     print(user_nerve_level, user_nerve_current, user_nerve_maximum)
 
     # notify user if necessary
-    if user_energy_level > target_level:
+    if 1.0 > user_energy_level >= target_level:
         await notify_user(target_user,
-                          f"Your energy is now {round(target_level*100, 3)}% full! "
+                          f"Your energy is now {user_energy_level_rounded}% full! "
                           f"({user_energy_current} / {user_energy_maximum})")
 
-    if user_nerve_level > target_level:
+    if 1.0 > user_nerve_level >= target_level:
         await notify_user(target_user,
-                          f"Your nerve is now {round(target_level*100, 3)}% full! "
+                          f"Your nerve is now {user_nerve_level_rounded}% full! "
                           f"({user_nerve_current} / {user_nerve_maximum})")
 
 
